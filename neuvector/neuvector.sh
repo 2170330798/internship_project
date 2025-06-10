09a70ef9ecbb7dc8d85c7b73b8bb9cdde459a3b1
2170330798 <2170330798@qq.com>
2025-06-10 09:58:53 +0800

    Good!

--- neuvector/neuvector.sh
+++ neuvector/neuvector.sh
@@ -10,18 +10,15 @@
 #   需要修改 neuvector-service-webui 的集群网络类型为NodePort
 #####################################################
 
-
-# 获取当前路径
-
-
 # 基础配置:
-export VERSION="5.3.2"
-export DockerHub="registry.docker.io:5000"
-export DockerHubIp="10.10.6.204"
-export SourceUrl="http://registry.docker.io/neuvector/kubernetes/${VERSION}/neuvector-k8s.yaml"
-export DestPath="$(pwd)/neuvector-k8s.yaml"
+VERSION="5.3.2"
+DockerHub="registry.docker.io:5000"
+DockerHubIp="10.10.6.204"
+SourceUrl="http://registry.docker.io/neuvector/kubernetes/${VERSION}/neuvector-k8s.yaml"
+DestPath="$(pwd)/neuvector-k8s.yaml"
 
 # 创建命名空间和需要的账号:
+echo "创建命名空间和sa用户认证证书:"
 kubectl create namespace neuvector
 kubectl create sa controller -n neuvector
 kubectl create sa enforcer -n neuvector
@@ -32,6 +29,7 @@ kubectl create sa registry-adapter -n neuvector
 
 # 提前下载neuvector所需镜像, 需要科学上网:
 # 官方镜像
+# echo "拉取镜像:"
 # docker pull neuvector/manager:${NEUVECTOR_VERSION}
 # docker pull neuvector/controller:${NEUVECTOR_VERSION}
 # docker pull neuvector/enforcer:${NEUVECTOR_VERSION}
@@ -40,12 +38,14 @@ kubectl create sa registry-adapter -n neuvector
 
 # 拉取后上传至你的私有仓库:
 # 重新打标签
+# echo "打标签:"
 # docker tag neuvector/manager:${NEUVECTOR_VERSION} ${DockerHub}/neuvector/manager:${NEUVECTOR_VERSION}
 # docker tag neuvector/controller:${NEUVECTOR_VERSION} ${DockerHub}/neuvector/controller:${NEUVECTOR_VERSION}
 # docker tag neuvector/enforcer:${NEUVECTOR_VERSION} ${DockerHub}/neuvector/enforcer:${NEUVECTOR_VERSION}
 # docker tag neuvector/scanner:latest ${DockerHub}/neuvector/scanner:latest
 # docker tag neuvector/updater:latest ${DockerHub}/neuvector/updater:latest
 # 上传镜像
+# echo "上传镜像:"
 # docker push ${DockerHub}/neuvector/manager:${NEUVECTOR_VERSION}
 # docker push ${DockerHub}/neuvector/controller:${NEUVECTOR_VERSION}
 # docker push ${DockerHub}/neuvector/enforcer:${NEUVECTOR_VERSION}
@@ -53,6 +53,7 @@ kubectl create sa registry-adapter -n neuvector
 # docker push ${DockerHub}/neuvector/updater:latest
 
 # 私有镜像:
+echo "拉取镜像:"
 docker pull ${DockerHub}/neuvector/manager:${NEUVECTOR_VERSION}
 docker pull ${DockerHub}/neuvector/controller:${NEUVECTOR_VERSION}
 docker pull ${DockerHub}/neuvector/enforcer:${NEUVECTOR_VERSION}
@@ -65,16 +66,20 @@ sed -i "$ a ${DockerHubIp}  ${DockerHub::-5}"  /etc/hosts
 # 下载官方的yaml部署文件(官方访问非常慢，搭建了一个私人的):
 wget ${SourceUrl} -O ${DestPath}
 
+echo "原始镜像:"
 cat ${DestPath} | grep image: 
 # 修改文件默认镜像
-sed -i 's@neuvector/manager:'${VERSION}'@'${DockerHu}'/neuvector/manager:'${VERSION}'@g' ${DestPath}
-sed -i 's@neuvector/controller:'${VERSION}'@'${DockerHub}'/neuvector/controller:'${VERSION}'@g' ${DestPath}
-sed -i 's@neuvector/enforcer:'${VERSION}'@r'${DockerHub}'/neuvector/enforcer:'${VERSION}'@g' ${DestPath}
-sed -i 's@neuvector/scanner:latest@'${DockerHub}'/neuvector/scanner:latest@g' ${DestPath}
-sed -i 's@neuvector/updater:latest@'${DockerHub}'/neuvector/updater:latest@g' ${DestPath}
+sed -i "s@neuvector/manager:${VERSION}@${DockerHub}/neuvector/manager:${VERSION}@g" ${DestPath}
+sed -i "s@neuvector/controller:${VERSION}@${DockerHub}/neuvector/controller:${VERSION}@g" ${DestPath}
+sed -i "s@neuvector/enforcer:${VERSION}@${DockerHub}/neuvector/enforcer:${VERSION}@g" ${DestPath}
+sed -i "s@neuvector/scanner:latest@${DockerHub}/neuvector/scanner:latest@g" ${DestPath}
+sed -i "s@neuvector/updater:latest@${DockerHub}/neuvector/updater:latest@g" ${DestPath}
 
+echo "更换后的镜像:"
 cat ${DestPath} | grep image: 
 
 # 部署neuvector
+echo "部署neuvector:"
 kubectl apply -f ${DestPath} 
 kubectl get svc -n neuvector
+kubectl get pods -n neuvector
